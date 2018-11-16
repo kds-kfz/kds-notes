b0b73c1c1da283200bab8c71b8c8a5254506e7a2
吴胤伶 <yinling.wu@inin88.com>
2018-11-16 10:19:25 +0800

    Feat(python): develop 修改17.prodcons.py文件

--- python/17.prodcons.py
+++ python/17.prodcons.py
@@ -4,56 +4,52 @@ import sys
 if(sys.version[:1] == "3"):import _thread as thread
 else:import thread
 
-from atexit import register
-from random import randrange
-from threading import Thread, BoundedSemaphore, Lock
-from time import sleep, ctime
+from random import randint
+from time import sleep
+from queue import Queue
+from myThread import MyThread
 
-lock = Lock()
-MAX = 5
-candytray = BoundedSemaphore(MAX)
+def writeQ(queue):
+    print('producing object for Q ...')
+    queue.put('xxx',1)
+    print('size now ' + str(queue.qsize()))
 
-def refill():
-    lock.acquire()
-    print('Refiling candy ...')
-    try:
-        candytray.release()
-    except ValueError:
-        print('full, skipping')
-    else:
-        print('OK')
-    lock.release()
+def readQ(queue):
+    val = queue.get(1)
+    print('consumed object from Q ... size now ' \
+            + str(queue.qsize()))
 
-def buy():
-    lock.acquire()
-    print('Buying candy ...')
-    if candytray.acquire(False):
-        print('OK')
-    else:
-        print('empty, skipping')
-    lock.release()
-
-def producer(loops):
+def writer(queue, loops):
     for i in range(loops):
-        refill()
-        sleep(randrange(3))
+        writeQ(queue)
+        sleep(randint(1, 3))
 
-def consumer(loops):
+def reader(queue, loops):
     for i in range(loops):
-        buy()
-        sleep(randrange(3))
+        readQ(queue)
+        sleep(randint(2, 5))
 
-def _main():
-    print('starting at : ' + ctime())
-    nloops = randrange(2, 6)
-    print('THE CANDY MACHINE (full with %d bars)!' % MAX)
-    Thread(target = consumer, args = (randrange(nloops, nloops + MAX +2),)).start()
-    Thread(target = producer, args = (nloops,)).start()
+funcs = [writer, reader]
+nfuncs = range(len(funcs))
 
-def _atexit():
-    print('all BONE at : ' + ctime())
+def main():
+    nloops = randint(2, 5)
+    q = Queue(32)
 
+    threads = []
+    for i in nfuncs:
+        t = MyThread(funcs[i], (q, nloops), funcs[i].__name__)
+        threads.append(t)
+
+    for i in nfuncs:
+        threads[i].start()
+
+    for i in nfuncs:
+        threads[i].join()
+
+    print('all DONE')
+            
 if __name__=='__main__':
-    _main()
+    main()
 
 
