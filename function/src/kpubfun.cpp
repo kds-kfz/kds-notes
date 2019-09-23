931bb698d7ee1303b05e06bba7245a56a58c9c61
吴胤伶 <651448672@qq.com>
2019-09-24 00:26:50 +0800

    Feat(c/c++): kpubfun 模块，拆分成 hpp 与 cpp，功能函数待测

---
+++ function/src/kpubfun.cpp
@@ -0,0 +1,296 @@
+#include"kpubfun.hpp"
+
+#include <cstdio>
+#include <cstring>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <vector>
+#include <sys/time.h>
+
+/* 2019年09月24日，对该文件重新设计，由于这是个功能函数文件，会有各种辅助功能函数，为方便管理故采用类中类设计 */
+
+//日期 静态成员变量，类外初始化
+time_t KPUBFUN::Date::tmt = {0};
+char KPUBFUN::Date::timeBuf[128] = {0};
+struct timeval KPUBFUN::Date::tv = {0};
+struct tm *KPUBFUN::Date::pTime = NULL;
+string KPUBFUN::Date::stime = "";
+
+//目录 静态成员变量，类外初始化
+DIR *KPUBFUN::DirFile::dir = NULL;
+struct dirent *KPUBFUN::DirFile::ptr = NULL;
+char KPUBFUN::DirFile::base[1024] = {0};
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 字 符 处 理 函 数 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+//demo："hello world read" --> "read"
+string KPUBFUN::CopyStringWithoutEmppty(char *dst, char *src, const char *flag){
+    #if 0
+    int len = strlen(src);
+    int pos = 0, tmppos = 0;
+    strncpy(dst, src, len);
+    string tmpstr(dst);
+    //不断更新 tmpstr 字符串，直到不在出现空格
+    //TODO 要是字符串太长，得不断更新 tmpstr
+    while(tmppos = tmpstr.find(' ') != string::npos && pos <len){
+        pos++;
+        tmpstr = string(dst + pos);
+    }
+    memset(dst, 0, len);
+    strncpy(dst, tmpstr.c_str(), tmpstr.size());
+    #endif
+    
+    string tmpstr(src);
+    int len = strlen(src);
+    string::size_type pos = 0;
+    memset(dst, 0, len);
+    if((pos = tmpstr.find_last_of(flag)) == string::npos){
+        strncpy(dst, tmpstr.c_str(), tmpstr.size());
+    }else{
+        tmpstr = tmpstr.substr(pos + 1);
+        strncpy(dst, tmpstr.c_str(), tmpstr.size());
+    }
+    return tmpstr;
+}
+
+//demo: 
+string KPUBFUN::StringMateOne(char *src, const char *mark, bool flag = true){
+    if(!strlen(src) && !strlen(mark))
+        return string("");
+    string tmpstr(src);
+    string::size_type pos = 0;
+    //从字符串后面查找，src 中 只要是出现 mark 中任何一个字符都算查找成功
+    pos = flag ? tmpstr.find_last_of(mark) : tmpstr.find_first_of(mark);
+    return pos == string::npos ? tmpstr : tmpstr.substr(pos + 1);
+}
+
+//demo: 
+string KPUBFUN::StringMateMoreLast(char *src, const char *mark, bool flag = true){
+    if(!strlen(src) && !strlen(mark))
+        return string("");
+    string tmpstr(src);
+    string::size_type pos = 0;
+    //从字符串后面查找，src 中 完全匹配 mark 字符串
+    pos = flag ? tmpstr.rfind(mark) : tmpstr.find(mark);
+    return pos == string::npos ? tmpstr : tmpstr.substr(pos + strlen(mark));
+}
+
+//demo: 
+string KPUBFUN::StringMateMoreFront(const char *src, const char *mark, bool flag = true){
+    if(!strlen(src) && !strlen(mark))
+        return string("");
+    string tmpstr(src);
+    string::size_type pos = 0;
+    //从字符串后面查找，src 中 完全匹配 mark 字符串
+    pos = flag ? tmpstr.rfind(mark) : tmpstr.find(mark);
+    return pos == string::npos ? tmpstr : tmpstr.substr(0, pos + strlen(mark));
+}
+
+//demo：StringSplit((char *)":12:::2:3:4:a::33::", ':', files)
+int KPUBFUN::StringSplit(char *src, const char split, vector<string> &files){
+    int start = 0;
+    int end = 0;
+    int nSpiltCount = 0;
+    char szTmp[256] = {0};
+    string szCurrent = "";
+
+    int nLength = strlen(src);
+    if(nLength == 0){
+        return 0;
+    }
+
+    for(int i = 0; i < nLength; i++){
+        if(split == src[i]){
+            //判断第一个分隔符出现前的字符和中间分隔符是否合法
+            if(i == 0 || (i > 0 && (split == src[i - 1]))){
+                start += 1;
+                continue;
+            }
+            nSpiltCount++;
+            end = i;
+            memcpy(szTmp, src + start, end - start);
+            files.push_back(string(szTmp));
+            start = end + 1;
+        }
+        memset(szTmp, 0, sizeof(szTmp));
+    }
+    //判断分隔符到结尾是否是有效字符
+    if(start != nLength){
+        memcpy(szTmp, src + start, nLength - start);
+        files.push_back(string(szTmp));
+    }
+    //TODO
+    //字符获取完成，字符有效校验待处理
+    //写一个函数可获取数字，字符，特殊符号的函数对其校验
+    return nSpiltCount;
+}
+
+//demo：
+int KPUBFUN::stringSplit( const string str, const string sep, vector <string> &vec )
+{
+    string::size_type begin, end;
+
+    if ( str.length() == 0 ){
+        return 0;
+    }
+
+    begin = 0;
+    while(1){
+        //end = str.find_first_of( sep, begin );
+        end = str.find( sep, begin );
+        if( end == string::npos ){
+            break;
+        }
+        vec.push_back( str.substr( begin, end-begin ) );
+        begin = end + sep.length();
+    }
+    vec.push_back( str.substr(begin) );
+
+    return vec.size();
+}
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 日 期 处 理 函 数 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+KPUBFUN::Date::Date(){
+    this->tmt = time(NULL);
+    this->pTime = NULL;
+    memset(timeBuf, 0, sizeof(timeBuf));
+    stime = "";
+    //this->ptv = NULL;
+    //this->ptz = NULL;
+}
+
+KPUBFUN::Date::~Date(){
+    pTime = NULL;
+}
+    
+//demo：%Y->年，%m->月，%d->日，%H->时，%M->分，%S->秒
+string KPUBFUN::Date::GetTime(const string& fmt){
+    pTime = localtime(&tmt);
+    strftime(timeBuf, sizeof(timeBuf), fmt.c_str(), pTime);
+    stime = timeBuf;
+    return stime;
+}
+    
+//demo：
+string KPUBFUN::Date::GetTime(){
+    pTime = localtime(&tmt);
+
+    gettimeofday(&tv, NULL);
+    //gettimeofday(&tv, ptz);
+    //localtime_r(&ptv->tv_sec, pTime);//线程安全
+    
+    //XXX 这里指针访问不到 tv_usec，不知为什么
+    //sprintf(timeBuf, "%04d-%02d-%02d %02d:%02d:%02d.%06ld",
+    //    pTime->tm_year + 1900, pTime->tm_mon + 1, pTime->tm_mday,
+    //    pTime->tm_hour, pTime->tm_min,pTime->tm_sec, ptv->tv_usec);
+        
+    sprintf(timeBuf, "%04d-%02d-%02d %02d:%02d:%02d.%06ld",
+        pTime->tm_year + 1900, pTime->tm_mon + 1, pTime->tm_mday,
+        pTime->tm_hour, pTime->tm_min, pTime->tm_sec, tv.tv_usec);
+    stime = timeBuf;
+    return stime;
+}
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 其 他 处 理 函 数 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+//demo：debugout("当前日期：%s", GetTime("%Y%m%d").c_str());
+void KPUBFUN::debugout(const char *fmt, ...){
+    char buf[512];
+    try{
+        va_list ap;
+        va_start(ap, fmt);
+        vsnprintf(buf, sizeof(buf) - 1, fmt, ap);
+        va_end(ap);
+        cout<<buf<<endl;
+    }catch(...){
+        cout<<"exception in construct debug message"<<endl;
+    }
+}
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 目 录 处 理 函 数 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+KPUBFUN::DirFile::DirFile(){
+    dir = NULL;
+    ptr = new dirent{0};
+    memset(base, 0, sizeof(base));
+}
+
+KPUBFUN::DirFile::~DirFile(){
+    closedir(dir);
+    delete []ptr;
+}
+
+//demo：readFileList(".", ".md", files);
+int KPUBFUN::DirFile::readFileList(const char *basePath, const char *mark, vector<string> &files, const char *head = NULL){
+    if((dir = opendir(basePath)) == NULL){
+        perror("Open dir error ...");
+        exit(1);
+    }
+    while((ptr = readdir(dir)) != NULL){
+        //跳过 . .. 以及隐藏文件或目录
+        if(strncmp(ptr->d_name, ".", 1) == 0 || strcmp(ptr->d_name, "..") == 0){
+            continue;
+        }else if(ptr->d_type == 8){//file
+            if(strstr(ptr->d_name, mark) != 0){
+                memset(base, '\0', sizeof(base));
+                strcpy(base, basePath);
+                strcat(base, "/");
+                if(head){
+                    if(strstr(ptr->d_name, head) != 0){
+                        strcat(base, ptr->d_name);
+                        files.push_back(base);
+                    }
+                    continue;
+                }
+                strcat(base, ptr->d_name);
+                files.push_back(base);
+            }
+        }else if(ptr->d_type == 10){//link file
+            cout<<"link file==>d_name:"<<basePath<<ptr->d_name<<endl;
+        }
+        else if(ptr->d_type == 4){//dir 是目录则递归调用
+            memset(base, '\0', sizeof(base));
+            strcpy(base, basePath);
+            strcat(base, "/");
+            strcat(base, ptr->d_name);
+            readFileList(base, mark, files);
+        }
+    }
+    closedir(dir);
+    return 0;
+}
+
+/*
+typedef struct task_node_t{
+    char AppSysCode[128];
+    char BranchCode[128];
+}task_node_t;
+
+int main(int argc, char *argv[]){
+    task_node_t node;
+    vector<string> files;
+    files.clear();
+    memset(&node, 0, sizeof(node));
+    char tempstr[64] = " hello world read";
+    CopyStringWithoutEmppty(node.AppSysCode, tempstr, sizeof(tempstr));
+    cout<<"["<<node.AppSysCode<<"]"<<endl;
+
+    readFileList("../log", ".log", files, "asdad-123");
+    cout<<"有效字符个数："<<StringSplit((char *)":12:::& $2:3:4:a::33::", ':', files)<<endl;
+    for(vector<string>::iterator it = files.begin(); it != files.end(); it++){
+        cout<<*it<<endl;
+    }
+    
+    string path = "../log/mount-service";
+    string yy = path.substr(0, path.find_last_of("/"));
+    cout<<yy<<endl;
+    yy = path.substr(path.find_last_of("/"));
+    cout<<yy<<endl;
+    
+    debugout("当前日期：%s", GetTime("%Y%m%d%H%M%S").c_str());
+    return 0;
+}
+*/
--- function/src/kpubfun.hpp
+++ function/src/kpubfun.hpp
@@ -1,307 +1,69 @@
 #ifndef __KPUBFUN__H__
 #define __KPUBFUN__H__
 
-#include <iostream>
 #include <string>
-
-#include <cstdio>
-#include <cstring>
-
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <vector>
-#include <sys/time.h>
 
 using namespace std;
 
-//namespace kfz{
-
-//字符串处理类
-class String{
-//类内，类外都可以访问
+class KPUBFUN{
 public:
-    /* **********************************************************************
-     * 函数功能：删除字符串中出现空格前的字符，前提是字符串中末尾不能出现空格
-     * demo："hello world read" --> "read"
-     * *********************************************************************/
-    string CopyStringWithoutEmppty(char *dst, char *src, const char *flag){
-        #if 0
-        int len = strlen(src);
-        int pos = 0, tmppos = 0;
-        strncpy(dst, src, len);
-        string tmpstr(dst);
-        //不断更新 tmpstr 字符串，直到不在出现空格
-        //TODO 要是字符串太长，得不断更新 tmpstr
-        while(tmppos = tmpstr.find(' ') != string::npos && pos <len){
-            pos++;
-            tmpstr = string(dst + pos);
-        }
-        memset(dst, 0, len);
-        strncpy(dst, tmpstr.c_str(), tmpstr.size());
-        #endif
-        
-        string tmpstr(src);
-        int len = strlen(src);
-        string::size_type pos = 0;
-        memset(dst, 0, len);
-        if((pos = tmpstr.find_last_of(flag)) == string::npos){
-            strncpy(dst, tmpstr.c_str(), tmpstr.size());
-        }else{
-            tmpstr = tmpstr.substr(pos + 1);
-            strncpy(dst, tmpstr.c_str(), tmpstr.size());
-        }
-        return tmpstr;
-    }
+    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 字 符 处 理 函 数 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+    
+    //功能描述: 删除字符串中出现空格前的字符，前提是字符串中末尾不能出现空格
+    static string CopyStringWithoutEmppty(char *dst, char *src, const char *flag);
+    //功能描述: 只匹配一个字符
+    static string StringMateOne(char *src, const char *mark, bool flag = true);
+    //功能描述: 完全匹配字符串，此函数兼容 StringMateOne 函数，知道后并返回 mark 之后的字符
+    static string StringMateMoreLast(char *src, const char *mark, bool flag = true);
+    //功能描述: 完全匹配字符串，此函数兼容 StringMateOne 函数，知道后并返回 mark 之前的字符
+    static string StringMateMoreFront(const char *src, const char *mark, bool flag = true);
+    //功能描述: 从字符串中截取字段到vector容器，分隔符是个字符，暂不支持字符字符有效校验
+    int StringSplit(char *src, const char split, vector<string> &files);
+    //功能描述: 从string中截取字段到vector容器
+    int stringSplit( const string str, const string sep, vector <string> &vec );
+    
+    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 其 他 处 理 函 数 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+    
+    //功能描述: 以任何格式打印出数据
+    static void debugout(const char *fmt, ...);
 
-    /* **********************************************************************
-     * 函数功能：只匹配一个字符
-     * demo: 
-     * *********************************************************************/
-    string StringMateOne(char *src, const char *mark, bool flag = true){
-        if(!strlen(src) && !strlen(mark))
-            return string("");
-        string tmpstr(src);
-        string::size_type pos = 0;
-        //从字符串后面查找，src 中 只要是出现 mark 中任何一个字符都算查找成功
-        pos = flag ? tmpstr.find_last_of(mark) : tmpstr.find_first_of(mark);
-        return pos == string::npos ? tmpstr : tmpstr.substr(pos + 1);
-    }
+    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 日 期 处 理 函 数 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+    
+    class Date{
+        private:
+            time_t tmt;
+            struct tm *pTime;
+            char timeBuf[128];
+            string stime;
+            struct timeval tv;
+            //struct timezone *ptz;
+        public:
+            Date();
+            ~Date();
 
-    /* **********************************************************************
-     * 函数功能：完全匹配字符串，此函数兼容 StringMateOne 函数，知道后并返回 mark 之后的字符
-     * demo: 
-     * *********************************************************************/
-    string StringMateMoreLast(char *src, const char *mark, bool flag = true){
-        if(!strlen(src) && !strlen(mark))
-            return string("");
-        string tmpstr(src);
-        string::size_type pos = 0;
-        //从字符串后面查找，src 中 完全匹配 mark 字符串
-        pos = flag ? tmpstr.rfind(mark) : tmpstr.find(mark);
-        return pos == string::npos ? tmpstr : tmpstr.substr(pos + strlen(mark));
-    }
+            //功能描述: 支持传入格式，比较灵活
+            static string GetTime(const string& fmt);
+            //功能描述: 获取本地时间
+            static string GetTime();
+    };
 
-    /* **********************************************************************
-     * 函数功能：完全匹配字符串，此函数兼容 StringMateOne 函数，知道后并返回 mark 之前的字符
-     * demo: 
-     * *********************************************************************/
-    string StringMateMoreFront(const char *src, const char *mark, bool flag = true){
-        cout<<"pos:++++"<<endl;
-        if(!strlen(src) && !strlen(mark))
-            return string("");
-        cout<<"pos:====="<<endl;
-        string tmpstr(src);
-        string::size_type pos = 0;
-        cout<<"pos:"<<pos<<endl;
-        //从字符串后面查找，src 中 完全匹配 mark 字符串
-        pos = flag ? tmpstr.rfind(mark) : tmpstr.find(mark);
-        return pos == string::npos ? tmpstr : tmpstr.substr(0, pos + strlen(mark));
-    }
+    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 目 录 处 理 函 数 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+    
+    class DirFile{
+        private:
+            DIR *dir;               //目录描述符，类似文件描述符：FILE *
+            struct dirent *ptr;     //目录指针
+            char base[1024];        //基目录
+        public:
+            DirFile();
+            ~DirFile();
 
-    //函数功能：从字符串中截取字段到vector容器，分隔符是个字符，暂不支持字符字符有效校验
-    //demo：StringSplit((char *)":12:::2:3:4:a::33::", ':', files)
-    int StringSplit(char *src, const char split, vector<string> &files){
-        int start = 0;
-        int end = 0;
-        int nSpiltCount = 0;
-        char szTmp[256] = {0};
-        string szCurrent = "";
-    
-        int nLength = strlen(src);
-        if(nLength == 0){
-            return 0;
-        }
-    
-        for(int i = 0; i < nLength; i++){
-            if(split == src[i]){
-                //判断第一个分隔符出现前的字符和中间分隔符是否合法
-                if(i == 0 || (i > 0 && (split == src[i - 1]))){
-                    start += 1;
-                    continue;
-                }
-                nSpiltCount++;
-                end = i;
-                memcpy(szTmp, src + start, end - start);
-                files.push_back(string(szTmp));
-                start = end + 1;
-            }
-            memset(szTmp, 0, sizeof(szTmp));
-        }
-        //判断分隔符到结尾是否是有效字符
-        if(start != nLength){
-            memcpy(szTmp, src + start, nLength - start);
-            files.push_back(string(szTmp));
-        }
-        //TODO
-        //字符获取完成，字符有效校验待处理
-        //写一个函数可获取数字，字符，特殊符号的函数对其校验
-        return nSpiltCount;
-    }
-    
-    //函数功能：从string中截取字段到vector容器
-    //demo：
-    int stringSplit( const string str, const string sep, vector <string> &vec )
-    {
-        string::size_type begin, end;
-    
-        if ( str.length() == 0 ){
-            return 0;
-        }
-    
-        begin = 0;
-        while(1){
-            //end = str.find_first_of( sep, begin );
-            end = str.find( sep, begin );
-            if( end == string::npos ){
-                break;
-            }
-            vec.push_back( str.substr( begin, end-begin ) );
-            begin = end + sep.length();
-        }
-        vec.push_back( str.substr(begin) );
-    
-        return vec.size();
-    }
-//只有在类内可以直接访问
-private:
-
-//类内、派生类可以访问；类外不能访问
-protected:
+            //功能描述: 读取目录下指定格式的所有文件路径到vector容器; 文件开头匹配, 初略的比较
+            static int readFileList(const char *basePath, const char *mark, vector<string> &files, const char *head = NULL);
+    };
 
 };
-
-//日期处理类
-class Date{
-private:
-    time_t tmt;
-    struct tm *pTime;
-    char timeBuf[128];
-    string stime;
-    
-    struct timeval tv;
-    //struct timezone *ptz;
-public:
-    Date(){
-        this->tmt = time(NULL);
-        this->pTime = NULL;
-        memset(timeBuf, 0, sizeof(timeBuf));
-        stime = "";
-        
-        //this->ptv = NULL;
-        //this->ptz = NULL;
-    }
-    
-    //支持传入格式，比较灵活
-    string GetTime(const string& fmt){
-        pTime = localtime(&tmt);
-        strftime(timeBuf, sizeof(timeBuf), fmt.c_str(), pTime);
-        stime = timeBuf;
-        return stime;
-    }
-    
-    //获取本地时间
-    string GetTime(){
-        pTime = localtime(&tmt);
-
-        gettimeofday(&tv, NULL);
-        //gettimeofday(&tv, ptz);
-        //localtime_r(&ptv->tv_sec, pTime);//线程安全
-        
-        //XXX 这里指针访问不到 tv_usec，不知为什么
-        //sprintf(timeBuf, "%04d-%02d-%02d %02d:%02d:%02d.%06ld",
-        //    pTime->tm_year + 1900, pTime->tm_mon + 1, pTime->tm_mday,
-        //    pTime->tm_hour, pTime->tm_min,pTime->tm_sec, ptv->tv_usec);
-            
-        sprintf(timeBuf, "%04d-%02d-%02d %02d:%02d:%02d.%06ld",
-            pTime->tm_year + 1900, pTime->tm_mon + 1, pTime->tm_mday,
-            pTime->tm_hour, pTime->tm_min, pTime->tm_sec, tv.tv_usec);
-        stime = timeBuf;
-        return stime;
-    }
-
-    ~Date(){
-        pTime = NULL;
-    }
-};
-
 #if 0
-//函数功能：以任何格式打印出数据
-//demo：debugout("当前日期：%s", GetTime("%Y%m%d").c_str());
-void debugout(const char *fmt, ...){
-    char buf[512];
-    try{
-        va_list ap;
-        va_start(ap, fmt);
-        vsnprintf(buf, sizeof(buf) - 1, fmt, ap);
-        va_end(ap);
-        cout<<buf<<endl;
-    }catch(...){
-        cout<<"exception in construct debug message"<<endl;
-    }
-}
-#endif
-
-//目录文件处理类
-class DirFile{
-private:
-    DIR *dir;           //目录描述符，类似文件描述符：FILE *
-    struct dirent *ptr; //目录指针
-    char base[1024];    //基目录
-public:
-    DirFile(){
-        dir = NULL;
-        ptr = new dirent{0};
-        memset(base, 0, sizeof(base));
-    }
-
-    //函数功能：读取目录下指定格式的所有文件路径到vector容器
-    //demo：readFileList(".", ".md", files);
-    //新增，文件开头匹配，初略的比较
-    int readFileList(const char *basePath, const char *mark, vector<string> &files, const char *head = NULL){
-        
-        if((dir = opendir(basePath)) == NULL){
-            perror("Open dir error ...");
-            exit(1);
-        }
-        while((ptr = readdir(dir)) != NULL){
-            //跳过 . .. 以及隐藏文件或目录
-            if(strncmp(ptr->d_name, ".", 1) == 0 || strcmp(ptr->d_name, "..") == 0){
-                continue;
-            }else if(ptr->d_type == 8){//file
-                if(strstr(ptr->d_name, mark) != 0){
-                    memset(base, '\0', sizeof(base));
-                    strcpy(base, basePath);
-                    strcat(base, "/");
-                    if(head){
-                        if(strstr(ptr->d_name, head) != 0){
-                            strcat(base, ptr->d_name);
-                            files.push_back(base);
-                        }
-                        continue;
-                    }
-                    strcat(base, ptr->d_name);
-                    files.push_back(base);
-                }
-            }else if(ptr->d_type == 10){//link file
-                cout<<"link file==>d_name:"<<basePath<<ptr->d_name<<endl;
-            }
-            else if(ptr->d_type == 4){//dir 是目录则递归调用
-                memset(base, '\0', sizeof(base));
-                strcpy(base, basePath);
-                strcat(base, "/");
-                strcat(base, ptr->d_name);
-                readFileList(base, mark, files);
-            }
-        }
-        closedir(dir);
-        return 0;
-    }
-    ~DirFile(){
-        closedir(dir);
-        delete []ptr;
-    }
-};
 //http通信类
 class Http{
 
@@ -331,7 +93,6 @@ class Xml{
 //监控处理
 
 //负载均衡
-
-//}
+#endif
 
 #endif
